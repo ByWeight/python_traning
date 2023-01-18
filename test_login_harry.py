35c4b0d2f91c9f12b1be5bd0e7b3b6276e210448
D4NS74R <97897939+d4ns74r@users.noreply.github.com>
2023-01-19 00:16:23 +0300

    Авторизация под юзером Harry Potter

--- test_login_harry.py
+++ test_login_harry.py
@@ -2,7 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 
-class TestTest():
+class TestTestloginharry():
     def setup_method(self, method):
         self.driver = webdriver.Chrome()
         self.vars = {}
@@ -10,9 +10,11 @@ class TestTest():
     def teardown_method(self, method):
         self.driver.quit()
 
-    def test_test(self):
-        self.driver.get("https://www.globalsqa.com/angularJs-protractor/BankingProject/#/login")
+    def test_testloginharry(self):
+        self.driver.get("https://www.globalsqa.com/angularJs-protractor/BankingProject/")
         self.driver.implicitly_wait(30)
-        self.driver.set_window_size(1680, 949)
         self.driver.find_element(By.CSS_SELECTOR, ".center:nth-child(1) > .btn").click()
-
+        self.driver.find_element(By.ID, "userSelect").click()
+        dropdown = self.driver.find_element(By.ID, "userSelect")
+        dropdown.find_element(By.XPATH, "//option[. = 'Harry Potter']").click()
+        self.driver.find_element(By.CSS_SELECTOR, ".btn-default").click()
