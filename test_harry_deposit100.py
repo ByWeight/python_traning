f5b8a1d57ebce73ab9084fb062bb3fda2a7e8755
D4NS74R <97897939+d4ns74r@users.noreply.github.com>
2023-01-19 19:45:03 +0300

    Небольшое улучшение кода

--- test_harry_deposit100.py
+++ test_harry_deposit100.py
@@ -2,23 +2,22 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 
-class TestTest():
+class Test():
     def setup_method(self, method):
         self.driver = webdriver.Chrome()
         self.vars = {}
+        self.driver.implicitly_wait(30)
 
     def teardown_method(self, method):
         self.driver.quit()
 
     def test_test(self):
-        self.driver.get("https://www.globalsqa.com/angularJs-protractor/BankingProject/")
-        self.driver.implicitly_wait(30)
-        self.driver.find_element(By.CSS_SELECTOR, ".center:nth-child(1) > .btn").click()
-        self.driver.find_element(By.ID, "userSelect").click()
-        dropdown = self.driver.find_element(By.ID, "userSelect")
-        dropdown.find_element(By.XPATH, "//option[. = 'Harry Potter']").click()
-        self.driver.find_element(By.CSS_SELECTOR, ".btn-default").click()
-        self.driver.find_element(By.CSS_SELECTOR, ".btn:nth-child(2)").click()
-        self.driver.find_element(By.CSS_SELECTOR, ".form-control").click()
-        self.driver.find_element(By.CSS_SELECTOR, ".form-control").send_keys("100")
-        self.driver.find_element(By.CSS_SELECTOR, ".btn-default").click()
+        self.driver.get('https://www.globalsqa.com/angularJs-protractor/BankingProject/')
+        self.driver.find_element(By.CSS_SELECTOR, 'button[ng-click*="customer()"]').click()
+        self.driver.find_element(By.CSS_SELECTOR, "select[class*=ng]").click()
+        self.driver.find_element(By.CSS_SELECTOR, '#userSelect > option:nth-child(3)').click()
+        self.driver.find_element(By.CSS_SELECTOR, '.btn-default').click()
+        self.driver.find_element(By.CSS_SELECTOR, '.btn:nth-child(2)').click()
+        self.driver.find_element(By.CSS_SELECTOR, '.form-control').click()
+        self.driver.find_element(By.CSS_SELECTOR, '.form-control').send_keys('100')
+        self.driver.find_element(By.CSS_SELECTOR, '.btn-default').click()
