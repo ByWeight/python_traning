c9d08da1b361e8761e7513a105131f5ba1010594
ByWeight <stdrum@mail.ru>
2023-01-20 19:50:17 +0300

    New test + changes

--- Test.py
+++ Test.py
@@ -1,3 +1,20 @@
-print('hello world 2.0')
-print('Say Hello!')
-print('gg nahoi')
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+
+class Testloginharry:
+    def setup_method(self):
+        self.driver = webdriver.Chrome()
+        self.vars = {}
+
+    def teardown_method(self):
+        self.driver.quit()
+
+    def test_loginharry(self):
+        self.driver.get("https://www.globalsqa.com/angularJs-protractor/BankingProject/")
+        self.driver.implicitly_wait(30)
+        self.driver.find_element(By.CSS_SELECTOR, ".center:nth-child(1) > .btn").click()
+        self.driver.find_element(By.ID, "userSelect").click()
+        dropdown = self.driver.find_element(By.ID, "userSelect")
+        dropdown.find_element(By.XPATH, "//option[. = 'Harry Potter']").click()
+        self.driver.find_element(By.CSS_SELECTOR, ".btn-default").click()
--- test_login_harry.py
+++ test_login_harry.py
@@ -2,15 +2,15 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 
-class TestTestloginharry():
-    def setup_method(self, method):
+class Testloginharry:
+    def setup_method(self):
         self.driver = webdriver.Chrome()
         self.vars = {}
 
-    def teardown_method(self, method):
+    def teardown_method(self):
         self.driver.quit()
 
-    def test_testloginharry(self):
+    def test_loginharry(self):
         self.driver.get("https://www.globalsqa.com/angularJs-protractor/BankingProject/")
         self.driver.implicitly_wait(30)
         self.driver.find_element(By.CSS_SELECTOR, ".center:nth-child(1) > .btn").click()
