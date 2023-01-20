d86eb2b2fbfef1ff546317a494e71c43f059d1b6
D4NS74R <97897939+d4ns74r@users.noreply.github.com>
2023-01-20 12:19:26 +0300

    Небольшое улучшение кода

--- test_login_harry.py
+++ test_login_harry.py
@@ -11,10 +11,10 @@ class TestTestloginharry():
         self.driver.quit()
 
     def test_testloginharry(self):
-        self.driver.get("https://www.globalsqa.com/angularJs-protractor/BankingProject/")
+        self.driver.get('https://www.globalsqa.com/angularJs-protractor/BankingProject/')
         self.driver.implicitly_wait(30)
-        self.driver.find_element(By.CSS_SELECTOR, ".center:nth-child(1) > .btn").click()
-        self.driver.find_element(By.ID, "userSelect").click()
-        dropdown = self.driver.find_element(By.ID, "userSelect")
-        dropdown.find_element(By.XPATH, "//option[. = 'Harry Potter']").click()
-        self.driver.find_element(By.CSS_SELECTOR, ".btn-default").click()
+        self.driver.find_element(By.CSS_SELECTOR, '.center:nth-child(1) > .btn').click()
+        self.driver.find_element(By.ID, 'userSelect').click()
+        dropdown = self.driver.find_element(By.ID, 'userSelect')
+        dropdown.find_element(By.XPATH, '//option[. = "Harry Potter"]').click()
+        self.driver.find_element(By.CSS_SELECTOR, '.btn-default').click()
