3acabc21a37f9f27ce7c82dfc13028f9b04ea2f5
ByWeight <stdrum@mail.ru>
2023-01-23 21:40:03 +0300

    Init comments

--- test_add_deposite.py
+++ test_add_deposite.py
@@ -12,14 +12,21 @@ class Test_AddDeposite():
         self.driver.quit()
 
     def test_test(self):
+        # Open Homepage
         self.driver.get('https://www.globalsqa.com/angularJs-protractor/BankingProject/')
         self.driver.set_window_size(1920, 1040)
+        # Chose you name
         self.driver.find_element(By.CSS_SELECTOR, 'button[ng-click*="customer()"]').click()
         self.driver.find_element(By.CSS_SELECTOR, "select[class*=ng]").click()
         self.driver.find_element(By.CSS_SELECTOR, '#userSelect > option:nth-child(3)').click()
+        # Authorize success
         self.driver.find_element(By.CSS_SELECTOR, '.btn-default').click()
         self.driver.find_element(By.CSS_SELECTOR, '.btn:nth-child(2)').click()
         self.driver.find_element(By.CSS_SELECTOR, '.form-control').click()
         self.driver.implicitly_wait(30)
         self.driver.find_element(By.CSS_SELECTOR, '.form-control').send_keys('500')
         self.driver.find_element(By.CSS_SELECTOR, '.btn-default').click()
+        # Logout
+        self.driver.find_element(By.CSS_SELECTOR, 'body > div > div > div.box.mainhdr > button.btn.logout').click()
+
+
