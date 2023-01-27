f56bf81caffc3c8e13f0a4c3d8ac5c88d5f4c46d
ByWeight <stdrum@mail.ru>
2023-01-27 19:26:51 +0300

    Road to pytest

--- test_add_deposite.py
+++ test_add_deposite.py
@@ -1,24 +1,27 @@
 from deposite import New_deposite
 from application import Application
+import pytest
 
 
-class Test_AddDeposite():
-    def setup(self):
-        self.app = Application()
+@pytest.fixture()
+def app(request):
+    fixture = Application
+    request.addfinalizer(fixture.destroy)
+    return fixture
 
-    def test_deposite_500(self):
-        self.app.open_home_page()
-        self.app.chose_name()
-        # Authorize success
-        self.app.add_deposite(New_deposite(add_deposite='500'))
-        self.app.logout()
 
-    def test_deposite_empty(self):
-        self.app.open_home_page()
-        self.app.chose_name()
-        # Authorize success
-        self.app.add_deposite(New_deposite(add_deposite='0'))
-        self.app.logout()
+def test_deposite_500(app):
+    app.open_home_page()
+    app.chose_name()
+    # Authorize success
+    app.add_deposite(New_deposite(add_deposite='500'))
+    app.logout()
 
-    def teardown(self):
-        self.app.destroy()
+
+def test_deposite_empty(app):
+    app.open_home_page()
+    app.chose_name()
+    # Authorize success
+    app.add_deposite(New_deposite(add_deposite='0'))
+    app.logout()
+
